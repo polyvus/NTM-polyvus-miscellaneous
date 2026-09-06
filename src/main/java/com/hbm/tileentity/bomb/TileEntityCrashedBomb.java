ea9dd4807f709820d6c2132a99f74f8ddf60740f
polyvus <bstsvulpus@gmail.com>
2026-09-06 11:06:12 +0200

    Update TileEntityCrashedBomb.java

--- src/main/java/com/hbm/tileentity/bomb/TileEntityCrashedBomb.java
+++ src/main/java/com/hbm/tileentity/bomb/TileEntityCrashedBomb.java
@@ -1,9 +1,12 @@
 package com.hbm.tileentity.bomb;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiConsumer;
 
 import com.hbm.blocks.bomb.BlockCrashedBomb.EnumDudType;
+import com.hbm.inventory.RecipesCommon.AStack;
+import com.hbm.tileentity.IRepairable;
 import com.hbm.util.ContaminationUtil;
 import com.hbm.util.EnumUtil;
 import com.hbm.util.ContaminationUtil.ContaminationType;
@@ -12,36 +15,112 @@ import com.hbm.util.ContaminationUtil.HazardType;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
 
-public class TileEntityCrashedBomb extends TileEntity {
-	
-	@Override
-	public void updateEntity() {
-		
-		if(!worldObj.isRemote) {
+public class TileEntityCrashedBomb extends TileEntity implements IRepairable {
 
-			if(worldObj.getTotalWorldTime() % 2 == 0) {
-				EnumDudType type = EnumUtil.grabEnumSafely(EnumDudType.class, this.getBlockMetadata());
-				
-				if(type == type.BALEFIRE)	affectEntities((entity, intensity) -> { ContaminationUtil.contaminate(entity, HazardType.RADIATION, ContaminationType.CREATIVE, 1F * intensity); }, 15D);
-				if(type == type.NUKE)		affectEntities((entity, intensity) -> { ContaminationUtil.contaminate(entity, HazardType.RADIATION, ContaminationType.CREATIVE, 0.25F * intensity); }, 10D);
-				if(type == type.SALTED)		affectEntities((entity, intensity) -> { ContaminationUtil.contaminate(entity, HazardType.RADIATION, ContaminationType.CREATIVE, 0.5F * intensity); }, 10D);
-			}
-		}
-	}
-	
-	public void affectEntities(BiConsumer<EntityLivingBase, Float> effect, double range) {
-		List<EntityLivingBase> list = worldObj.getEntitiesWithinAABB(EntityLivingBase.class, AxisAlignedBB.getBoundingBox(xCoord + 0.5, yCoord + 0.5, zCoord + 0.5, xCoord + 0.5, yCoord + 0.5, zCoord + 0.5).expand(range, range, range));
-		for(EntityLivingBase entity : list) {
-			double dist = Math.sqrt(getDistanceFrom(entity.posX, entity.posY + entity.height / 2, entity.posZ));
-			if(dist > range) continue;
-			float intensity = (float) (1D - dist / range);
-			effect.accept(entity, intensity);
-		}
-	}
-	
-	@Override public AxisAlignedBB getRenderBoundingBox() { return TileEntity.INFINITE_EXTENT_AABB; }
-	@Override @SideOnly(Side.CLIENT) public double getMaxRenderDistanceSquared() { return 65536.0D; }
+    public boolean isRepaired = false;
+
+    @Override
+    public void updateEntity() {
+
+        if(!worldObj.isRemote) {
+
+            if(worldObj.getTotalWorldTime() % 2 == 0) {
+                EnumDudType type = EnumUtil.grabEnumSafely(EnumDudType.class, this.getBlockMetadata());
+
+                if(!isRepaired) {
+                    if(type == type.BALEFIRE)
+                        affectEntities((entity, intensity) -> {
+                            ContaminationUtil.contaminate(entity, HazardType.RADIATION,
+                                    ContaminationType.CREATIVE, 1F * intensity);
+                        }, 15D);
+
+                    if(type == type.NUKE)
+                        affectEntities((entity, intensity) -> {
+                            ContaminationUtil.contaminate(entity, HazardType.RADIATION,
+                                    ContaminationType.CREATIVE, 0.25F * intensity);
+                        }, 10D);
+
+                    if(type == type.SALTED)
+                        affectEntities((entity, intensity) -> {
+                            ContaminationUtil.contaminate(entity, HazardType.RADIATION,
+                                    ContaminationType.CREATIVE, 0.5F * intensity);
+                        }, 10D);
+                }
+            }
+        }
+    }
+
+    public void affectEntities(BiConsumer<EntityLivingBase, Float> effect, double range) {
+        List<EntityLivingBase> list = worldObj.getEntitiesWithinAABB(
+                EntityLivingBase.class,
+                AxisAlignedBB.getBoundingBox(
+                        xCoord + 0.5,
+                        yCoord + 0.5,
+                        zCoord + 0.5,
+                        xCoord + 0.5,
+                        yCoord + 0.5,
+                        zCoord + 0.5
+                ).expand(range, range, range)
+        );
+
+        for(EntityLivingBase entity : list) {
+            double dist = Math.sqrt(
+                    getDistanceFrom(
+                            entity.posX,
+                            entity.posY + entity.height / 2,
+                            entity.posZ
+                    )
+            );
+
+            if(dist > range)
+                continue;
+
+            float intensity = (float) (1D - dist / range);
+            effect.accept(entity, intensity);
+        }
+    }
+
+    @Override
+    public boolean isDamaged() {
+        return !isRepaired;
+    }
+
+    @Override
+    public List<AStack> getRepairMaterials() {
+        return new ArrayList<AStack>();
+    }
+
+    @Override
+    public void repair(EntityPlayer player) {
+        isRepaired = true;
+        markDirty();
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound nbt) {
+        super.readFromNBT(nbt);
+        isRepaired = nbt.getBoolean("isRepaired");
+    }
+
+    @Override
+    public void writeToNBT(NBTTagCompound nbt) {
+        super.writeToNBT(nbt);
+        nbt.setBoolean("isRepaired", isRepaired);
+    }
+
+    @Override
+    public AxisAlignedBB getRenderBoundingBox() {
+        return TileEntity.INFINITE_EXTENT_AABB;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public double getMaxRenderDistanceSquared() {
+        return 65536.0D;
+    }
 }
